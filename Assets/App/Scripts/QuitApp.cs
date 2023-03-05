6d4c1cd6933a6a6fc841df614ea4d53a7283da71
marko-andlar <markan13@windowslive.com>
2023-03-05 18:41:09 +0100

    Implement quit button

---
+++ Assets/App/Scripts/QuitApp.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace App.Scripts
+{
+    public class QuitApp : MonoBehaviour
+    {
+        public void Quit()
+        {
+            Application.Quit();
+        }
+    }
+}
