7bf99e57e9e3399717fd0c5b2cb8a732194c77d7
marko-andlar <markan13@windowslive.com>
2023-03-05 12:33:18 +0100

    Add bird info UI

---
+++ Assets/App/Scripts/CanvasGroupExtension.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace App.Scripts
+{
+    [RequireComponent(typeof(CanvasGroup))]
+    public class CanvasGroupExtension : MonoBehaviour
+    {
+        private CanvasGroup _canvasGroup;
+
+        private void Awake()
+        {
+            _canvasGroup = GetComponent<CanvasGroup>();
+            if (_canvasGroup == null) Debug.LogError("CanvasGroup component is missing");
+        }
+
+        public void SetActive(bool isActive)
+        {
+            _canvasGroup.alpha = isActive ? 1 : 0;
+            _canvasGroup.interactable = isActive;
+            _canvasGroup.blocksRaycasts = isActive;
+        }
+    }
+}
